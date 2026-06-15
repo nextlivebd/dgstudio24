5a0b4dfd7dacb9fde34a72f64ab936a0ff831e1d
Md Alamgir Hossain <per.nahid@gmail.com>
2026-06-15 12:23:02 +0600

    Fix 419 error on cPanel by trusting proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->append(\App\Http\Middleware\TrackVisitor::class);
 
         $middleware->alias([
